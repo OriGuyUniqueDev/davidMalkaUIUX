45693b60357b5f415902d0f820dc91c29f4b2617
OriGuyUniqueDev <origuydev@gmail.com>
2022-10-08 23:23:56 +0300

    add basics cards

---
+++ src/components/HomepageCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="mainWrapper flex flex-col hover:scale-110">
+        <img v-bind:src="url" alt="">
+        <h1>{{title}}</h1>
+        <h2>{{role}}</h2>
+        <button type="button">View project</button>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps(['title',"role","url"])
+    console.log(props.url);
+</script>
+
+<style scoped>
+.mainWrapper{
+    transition: all 0.375s linear;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,20 +5,47 @@
 			<ProfilePic></ProfilePic> 
 		</div>
 	</div>
+	<div class="text-white flex flex-col ">
+		<HomepageCard v-for="card in cardsData" :key="card.title" :title="card.title" :role="card.role" :url="card.url"></HomepageCard>
+	</div>
 </template>
 
 <script setup>
 	import HeroText from "../components/HeroText.vue";
 	import ProfilePic from "../components/ProfilePic.vue";
+import HomepageCard from "../components/HomepageCard.vue";
+
+	const cardsData = [
+		{
+			title:"Pa’amonim",
+			role: "UI/UX Design",
+			url: "src\\assets\\images\\paamonim.png"
+		},
+		{
+			title:"Ram Aderet",
+			role: "Product Design",
+			url: "src/assets/images/ramAderet.png"
+		},
+		{
+			title:"Patricia",
+			role: "Product Design",
+			url: "src/assets/images/patricia.png"
+		},
+		{
+			title:"Abra Web & Mobile",
+			role: "UI/UX Design",
+			url: "src/assets/images/Abra_Web_Mobile.png"
+		}
+	]
 </script>
 
 <style lang="css" scoped>
-	.mainWrapper {
-		position: absolute;
+	/* .mainWrapper {
+		position: relative;
 		top: 9.5rem;
 		left: 0;
 		right: 0;
 		bottom: 0;
 		padding: 0 2rem;
-	}
+	} */
 </style>
