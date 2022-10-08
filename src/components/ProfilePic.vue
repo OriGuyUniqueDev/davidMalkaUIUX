eb9785a35444be3cf950d51ca9ce5c68f265c8a3
OriGuyUniqueDev <origuydev@gmail.com>
2022-10-08 13:06:43 +0300

    update profile image url

--- src/components/ProfilePic.vue
+++ src/components/ProfilePic.vue
@@ -2,15 +2,15 @@
 	<div>
 		<img class="
         w-[43.06rem] h-[38.6rem] left-[-70%] relative -top-20
-        sm:w-[60rem] sm:h-[57.6rem] sm:left-0 sm: top-0
+        sm:w-[60rem] sm:h-[57.6rem] sm:left-0 
         md:w-[72.1rem] md:h-[69.2rem]
         " 
-        :src="imgUrl" alt="profile picture" />
+        src="@/assets/images/profilePic.png" alt="profile picture" />
 	</div>
 </template>
 
 <script setup>
-	let imgUrl = "@/assets/photos/profilePic.png";
+	
 </script>
 
 <style lang="css" scoped>
