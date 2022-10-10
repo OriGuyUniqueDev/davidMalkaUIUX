17a8fa4b49403a39e537c1a0ba7395ee84423f1e
OriGuyUniqueDev <origuydev@gmail.com>
2022-10-10 11:46:46 +0300

    add attribute

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Nav from './components/Nav.vue';
 import Footer from './components/Footer.vue';
+import Developer from './components/Developer.vue';
 
 </script>
 
 <template>
   <Nav></Nav>
   <Footer></Footer>
+  <Developer></Developer>
 </template>
 
 <style scoped>
---
+++ src/components/Developer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class=" mainWrapper gap-3 py-2 text-white bg-black">
+        <div class="text-xl  ">Developed by: </div>
+         <img class="w-[12rem]" src="@/assets/images/brandmark-design (3).png" alt="">
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.mainWrapper{
+    display: flex;
+    justify-content: center;
+}
+</style>
