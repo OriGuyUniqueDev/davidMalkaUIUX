869f085efa5d9ddbc569638a3d11fd8b3b683287
OriGuyUniqueDev <origuydev@gmail.com>
2022-10-10 12:12:23 +0300

    change logo padding

--- src/components/Developer.vue
+++ src/components/Developer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class=" mainWrapper text-greyText gap-3 py-2 text-white bg-black">
+    <div class=" mainWrapper text-greyText gap-3 py-6 text-white bg-black">
         <div class="text-xl  ">Developed by: </div>
          <img class="w-[10rem] lg:w-[12rem]" src="@/assets/images/logo.png" alt="logo">
     </div>
