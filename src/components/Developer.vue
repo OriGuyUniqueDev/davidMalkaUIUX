a17554d7afc3c25975bec44bf556d03e937deed5
OriGuyUniqueDev <origuydev@gmail.com>
2022-10-10 12:05:59 +0300

    .

--- src/components/Developer.vue
+++ src/components/Developer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class=" mainWrapper gap-3 py-2 text-white bg-black">
         <div class="text-xl  ">Developed by: </div>
-         <img class="w-[12rem]" src="@/assets/images/brandmark-design (3).png" alt="logo">
+         <img class="w-[12rem]" src="@/assets/images/logo.png" alt="logo">
     </div>
 </template>
 
